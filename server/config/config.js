dd6b0c8c62d598d2c79742c3e7adcbcd90739cac
Alejandro <alejandro.lvd@gmail.com>
2019-03-18 12:58:21 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,18 @@
-
-
 // ==========================================
 // Puerto
 // ==========================================
 process.env.PORT = process.env.PORT || 3000
+
+// ==========================================
+// Entorno
+// ==========================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+let urlDB
+if (process.env.NODE_ENV === 'dev') {
+  urlDB = 'mongodb://localhost:27017/cafe'
+}
+else {
+  const pass = 'SkvmhOZOIg3AU3ah', usr = 'alejandro'
+  urlDB = `mongodb+srv://${usr}:${pass}@cluster0-lsljf.mongodb.net/cafe?retryWrites=true`
+}
+process.env.URLDB = urlDB
---
+++ server/routes/usuario.js
@@ -0,0 +1,96 @@
+const express = require('express')
+const app = express()
+const Usuario = require('../models/usuario')
+const bcrypt = require('bcrypt')
+const _ = require('lodash')
+
+app.get('/usuario', function (req, res) {
+  let desde = Number(req.query.desde) || 0
+  let cuantos = Number(req.query.cuantos) || 5
+  Usuario.find({estado: true}, 'nombre email role estado google img')
+    .skip(desde)
+    .limit(cuantos)
+    .exec((err, usuarios) => {
+      if (err) {
+        return res.status(400).json({
+          ok: false,
+          err
+        })
+      }
+      Usuario.count({estado: true}, (err, total) => {
+        res.json({
+          ok: true,
+          total,
+          usuarios
+        })
+      })
+    })
+})
+
+app.post('/usuario', function (req, res) {
+  let body = req.body
+
+  let usuario = new Usuario({
+    nombre: body.nombre,
+    email: body.email,
+    password: bcrypt.hashSync(body.password, 10),
+    role: body.role
+  })
+
+  usuario.save((err, usuarioDB) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err
+      })
+    }
+    res.json(usuarioDB)
+  })
+})
+
+app.put('/usuario/:id', function (req, res) {
+  let id = req.params.id
+  let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado'])
+  
+  Usuario.findByIdAndUpdate(id, body, {
+    new: true,
+    runValidators: true
+  }, (err, usuarioDB) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err
+      })
+    }
+    res.json({
+      ok: true,
+      usuario: usuarioDB
+    })
+  })
+})
+
+app.delete('/usuario/:id', function (req, res) {
+  let id = req.params.id
+  Usuario.findByIdAndUpdate(id, {estado: false}, (err, usuarioDB) => {
+    if (err) {
+      return res.status(400).json({
+        ok: false,
+        err
+      })
+    }
+    if (!usuarioDB || !usuarioDB.estado) {
+      return res.status(400).json({
+        ok: false,
+        err: {
+          message: 'Usuario no encontrado'
+        }
+      })
+    }
+    res.json({
+      ok: true,
+      usuario: usuarioDB
+    })
+  })
+})
+
+module.exports = app
